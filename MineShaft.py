e42b2ea2da86eb29faef2f41b601340c49a40c4e
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-05-04 00:06:13 +0800

    Copy API from Gym.Env, to be changed for MineShaft

--- MineShaft.py
+++ MineShaft.py
@@ -1,27 +1,52 @@
-import cv2
-import pyautogui
-import numpy as np
-from mss.windows import MSS as mss
+"""This is the base class of the MineShaft environment. It inherited OpenAI Gym Environment.
 
+For any game supported by MineShaft, the environment inherited this class as an interface.
+"""
+from enum import Enum
 
-class MineShaft:
-    def __init__(self):
-        self.sct=mss()
-		width, height= pyautogui.size()
-		monitor = {
-			"top": (height-360)//2,
-			"left": (width-640)//2,
-			"width": 640,
-			"height": 360
-		}
-        # start the game
+import gym
+from gym import spaces
+
+class BaseEnv(gym.Env):
+    """Custom Environment that follows gym interface
+    
+    Attributes
+    ----------
+    IO_MODE : Enum
+        IO_MODE.API = direct access to game API
+	IO_MODE.SIMPLIFIED = simpilified observation_space and action_space
+	IO_MODE.FULL_CONTROL = screen and full keyboard and mouse control
+    """
+    metadata = {'render.modes': ['human']}
+    IO_MODE = Enum('IO_MODE', ('API', 'SIMPLIFIED', 'FULL_CONTROL'))
+
+    def __init__(self, io_mode=IO_MODE.FULL_CONTROL):
+        super(CustomEnv, self).__init__()
+        # Define action and observation space
+        # They must be gym.spaces objects
+        # Example when using discrete actions:
+        self.action_space = spaces.Discrete(N_DISCRETE_ACTIONS)
+        # Example for using image as input:
+        self.observation_space = spaces.Box(low=0, high=255,
+                                            shape=(HEIGHT, WIDTH, N_CHANNELS), dtype=np.uint8)
 
     def step(self, action):
         self._take_action(action)
         reward = self._get_reward()
         ob = np.array(sct.grab(monitor))
         episode_over = False
-        return ob, reward, episode_over, {}
+	info = {}
+        return observation, reward, done, info
+
+    def reset(self):
+        ...
+        return observation  # reward, done, info can't be included
+
+    def render(self, mode='human'):
+        pass
+
+    def close (self):
+        ...
 
     def _reset(self):
         pass
