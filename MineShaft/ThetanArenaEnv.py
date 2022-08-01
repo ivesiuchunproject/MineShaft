fc2c59cdc6ad1e9831c8a4caea5f111ef9b2ab92
TongTszHo <110086299+TongTszHo@users.noreply.github.com>
2022-08-01 16:09:25 +0800

    Update _start_game
    I add the code that hardcode the path to start game.

--- MineShaft/ThetanArenaEnv.py
+++ MineShaft/ThetanArenaEnv.py
@@ -49,8 +49,9 @@ class ThetanArenaEnv(BaseEnv):
     
     def _start_game(self):
         """This is an example for how to open the Thetan Areana.exe.
+        
         The path "C:\\Program Files (x86)\\Thetan Arena\\Thetan Arena.exe" is hardcode.
-    """
+        """
         os.startfile('"C:\\Program Files (x86)\\Thetan Arena\\Thetan Arena.exe"')
         
     
