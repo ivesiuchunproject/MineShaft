59a0d32f897b3c619b174edef0873e5363818199
Stupid <22520563+NewJerseyStyle@users.noreply.github.com>
2022-08-02 08:13:31 +0800

    Added comments for test-case mouse-move

--- test/test_ThetanArenaEnv.py
+++ test/test_ThetanArenaEnv.py
@@ -14,6 +14,15 @@ class ThetanArenaEnvTestCase(unittest.TestCase):
 		self.args = None
 
 	def test__mouse_move(self):
+		"""Test case for mouse move method
+		
+		The function will call mouse move method of `ThetanArenaEnv` with
+		parameters `x=0.3, y=0.6` of the screen. Then check if the mouse
+		coordinate now moved to the expected `(x, y)` location.
+		
+		(Cannot test correctly if `(x, y)` definition changed to reletive
+		coordinate of game window)
+		"""
 		expected = np.asarray(pyautogui.size()) * np.asarray(self.args);
 		self.env._mouse_click(*self.args);
 		result = np.asarray(pyautogui.position());
